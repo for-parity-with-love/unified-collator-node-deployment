13821f3079616c8325810e99785ecc1374b7ae8a
babebort <babebort@gmail.com>
2023-02-02 13:37:42 +0800

    fix: internal issue

--- AWS/label.tf
+++ AWS/label.tf
@@ -2,15 +2,23 @@ module "label" {
   source  = "cloudposse/label/null"
   version = "0.25.0"
 
-  environment   = "${local.environment}"
-  name      = var.project_name
+  environment = local.environment
+  name        = var.project_name
 }
 
 module "vpc_label" {
   source  = "cloudposse/label/null"
   version = "0.25.0"
 
+  attributes  = ["vpc"]
+  environment = local.environment
+  name        = var.project_name
+}
 
-  environment   = "${local.environment}"
-  name      = var.project_name
+module "ec2_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
+  environment = local.environment
+  tenant      = var.project_name
 }
--- AWS/local.tf
+++ AWS/local.tf
@@ -1,22 +1,5 @@
 locals {
   environment             = lower(terraform.workspace)
-
-  public_subnets_additional_tags = {
-    "kubernetes.io/role/elb" : 1
-    "kubernetes.io/cluster/${local.environment}-${var.project_name}-cluster" = "shared"
-  }
-  private_subnets_additional_tags = {
-    "kubernetes.io/role/internal-elb" : 1
-    "kubernetes.io/cluster/${local.environment}-${var.project_name}-cluster" = "shared"
-  }
-
-  tags = {
-    "kubernetes.io/cluster/${local.environment}-${var.project_name}-cluster" = "shared"
-    "kubernetes.io/cluster/${local.environment}-${var.project_name}-cluster" = "shared"
-    "Project"   = "${var.project_name}"
-    "Environment" = "${local.environment}"
-  }
-
   account_id = data.aws_caller_identity.current.account_id
 }
 
